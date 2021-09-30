82da2d8a28770e36bb28b1e9360587f4225c2910
fncnt <>
2021-09-30 12:30:16 +0200

    Barebone structure for the project

---
+++ src/autocorrelation.rs
@@ -0,0 +1,1 @@
+use rustfft::FftPlanner;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+#![warn(missing_docs)]
+#![warn(rustdoc::missing_crate_level_docs)]
+
+//! Rust implementation of [`RAFFT`](https://www.biorxiv.org/content/10.1101/2021.07.02.450908v1.full)
+//! TODO: crate level documentation
+
+/// Autocorrelation of an encoded RNA sequence using FFT
+#[allow(dead_code)]
+pub mod autocorrelation;
+/// Encoding of RNA sequences using nucleotide representations suitable for FFT
+#[allow(dead_code)]
+pub mod encoding;
+//#[cfg(feature = "rna")]
+//#[allow(dead_code)]
+//pub mod optionalmodule;
+
+#[cfg(feature = "bindings")]
+use pyo3::prelude::*;
+
+/// TODO
+#[cfg(feature = "bindings")]
+#[pymodule]
+fn rafft(_py: Python, m: &PyModule) -> PyResult<()> {
+    /// TODO documentation and API
+    #[pyfn(m)]
+    fn fold(_py: Python, sequence: &str) -> PyResult<(f64, String)> {
+        unimplemented!()
+        //Ok(crate_fold(s1, s2).unwrap())
+    }
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
