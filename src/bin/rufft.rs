8ad6cdd1eeec167bab2b8af434446d5bd1d3be32
fncnt <git@vncnt.eu>
2021-10-21 18:51:31 +0200

    MVP commandline interface

--- src/bin/rufft.rs
+++ src/bin/rufft.rs
@@ -1,7 +1,8 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
 
-use rafft::*;
+use rafft::fast_folding::RafftConfig;
+use rafft::{set_global_energy_parameters, set_global_temperature};
 
 #[derive(StructOpt, Debug)]
 #[structopt(
@@ -18,14 +19,75 @@ struct Opt {
     parameters: Option<PathBuf>,
     #[structopt(help = "input RNA sequence")]
     sequence: String,
-    #[structopt(long = "AU", help = "Weight of AU base pairs", default_value = "1.0")]
+    #[structopt(long = "AU", help = "Weight of AU base pairs", default_value = "2.0")]
     au: f64,
-    #[structopt(long = "GC", help = "Weight of GC base pairs", default_value = "1.0")]
+    #[structopt(long = "GC", help = "Weight of GC base pairs", default_value = "3.0")]
     gc: f64,
     #[structopt(long = "GU", help = "Weight of GU base pairs", default_value = "1.0")]
     gu: f64,
+    #[structopt(
+        long = "temperature",
+        short = "T",
+        help = "Temperature in °C, passed to ViennaRNA",
+        default_value = "37.0"
+    )]
+    temperature: f64,
+    #[structopt(
+        long = "min-unpaired",
+        short = "u",
+        help = "Minimum amount of unpaired positions enclosed by a hairpin loop",
+        default_value = "3"
+    )]
+    min_unpaired: usize,
+    #[structopt(
+        long = "minimum-helix-energy",
+        short = "e",
+        help = "Minimum energy candidate helix stacks have to contribute; lower (negative) is more stable",
+        default_value = "0.0"
+    )]
+    min_loop_energy: f64,
+    #[structopt(
+        long = "positional-lags",
+        short = "l",
+        help = "Minimum amount of unpaired positions enclosed by a hairpin loop",
+        default_value = "100"
+    )]
+    positional_lags: usize,
+    #[structopt(
+        long = "branch",
+        short = "b",
+        help = "Number of branches to explore in construction of the fast-folding graph",
+        default_value = "1000"
+    )]
+    number_of_branches: usize,
+    #[structopt(
+        long = "saved-trajectories",
+        short = "s",
+        help = "Amount of saved structures per step of the breadth-first search",
+        default_value = "1"
+    )]
+    saved_trajectories: usize,
 }
 
 fn main() {
     let opt = Opt::from_args();
+
+    if let Some(path) = opt.parameters {
+        set_global_energy_parameters(path);
+    }
+
+    #[allow(clippy::float_cmp)]
+    if opt.temperature != 37.0 {
+        set_global_temperature(opt.temperature);
+    }
+
+    let rafft_config = RafftConfig::new()
+        .maximum_trajectories(opt.saved_trajectories)
+        .basepair_weights(opt.au, opt.gc, opt.gu)
+        .minimum_unpaired_in_hairpins(opt.min_unpaired)
+        .minimum_loop_energy(opt.min_loop_energy)
+        .maximum_branches(opt.number_of_branches)
+        .positional_lags(opt.positional_lags);
+
+    rafft_config.fold(&opt.sequence); //TODO: this method will need to be adjusted in the near future
 }
